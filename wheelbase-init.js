aa4e25ee07a8ae405679329d7d576148e752ddae
vanishtravel <89663621+vanishtravel@users.noreply.github.com>
2023-05-03 11:26:17 -0400

    Update wheelbase-init.js

--- wheelbase-init.js
+++ wheelbase-init.js
@@ -1,15 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var wheelbase = Wheelbase({
-    accountId: 1969389,
-    urlParams: true,
-    currency: "USD",
+document.addEventListener("DOMContentLoaded", function() {
+  const wheelbase = new Wheelbase({
+    domain: "https://www.vanishtravel.com",
+    apiKey: " dev_key", // Replace "your-api-key" with your actual API key
   });
 
   wheelbase.addWidget(wheelbase.widgets.DateRange, "availability-calendar-container", {
     rentalId: 221183,
     embedded: true,
-    numberOfMonths: 2, // optional
+    numberOfMonths: 2,
   });
-
-  wheelbase.start();
 });
