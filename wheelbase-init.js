a5582cfc15ca8f69950ec309b149fbafa9d7a4f2
vanishtravel <89663621+vanishtravel@users.noreply.github.com>
2023-05-03 11:48:35 -0400

    Update wheelbase-init.js

--- wheelbase-init.js
+++ wheelbase-init.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
   new Wheelbase({ accountId: 1969389 });
-});
-
 
   wheelbase.addWidget(wheelbase.widgets.DateRange, "availability-calendar-container", {
     rentalId: 221183,
